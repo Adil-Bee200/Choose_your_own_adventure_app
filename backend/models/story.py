f820b54c53aa687d13b6d77f8806874b83a5a3a8
Adil-Bee200 <151982354+Adil-Bee200@users.noreply.github.com>
2025-08-11 18:03:08 +0500

    Created Story and StoryNode sqlalchemy models

---
+++ backend/models/story.py
@@ -0,0 +1,30 @@
+from sqlalchemy import Column, String, Integer, DateTime, Boolean, ForeignKey, JSON
+from sqlalchemy.sql import func 
+from sqlalchemy.orm import relationship
+from backend.db.databases import base
+
+## Contains metadata about the overall story
+class Story(base):
+    __tablename__ = "stories"
+    id = Column(Integer, primary_key=True, Index= True)
+    title = Column(String, Index=True)
+    session_id = Column(String, Index= True)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    
+    nodes = relationship("StoryNode", back_populates="story")  ## one to many relationship (one story connected to many nodes)
+
+
+## Nodes contains all the different options and paths for story
+class StoryNode(base):
+    __tablename__ = "story_nodes"
+
+    id = Column(Integer, primary_key=True, index=True)
+    story_id = Column(Integer, ForeignKey("stories.id"), index=True)
+    content = Column(String)
+    is_root = Column(Boolean, default=False)
+    is_ending = Column(Boolean, default=False)
+    is_winning_ending = Column(Boolean, default=False)
+    options = Column(JSON, default=list)
+
+    story = relationship("Story", "nodes")
+
