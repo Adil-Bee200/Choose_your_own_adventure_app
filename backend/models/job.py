b1d21e6544c2745f09758704e00653577fe90eff
Adil-Bee200 <151982354+Adil-Bee200@users.noreply.github.com>
2025-08-11 18:10:28 +0500

    Created StoryJob sqlalchemy model

---
+++ backend/models/job.py
@@ -0,0 +1,18 @@
+from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy.sql import func
+
+from backend.db.databases import base
+
+## StoryJob represents status of a story being made (LLM will take time to acctually make the story)
+class StoryJob(base):
+    __tablename__ = "story_job"
+
+    id = Column(Integer, primary_key=True, index=True)
+    job_id = Column(String, index= True, unique= True)
+    session_id = Column(String, index=True)
+    theme = Column(String)
+    status = Column(String)
+    story_id = Column(Integer, nullable=True)
+    error = Column(String, nullable=True)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    completed_at = Column(DateTime(timezone=True), nullable=True)             ## Job could fail to complete
