c05347bb40e818785b5e6cc029d78bee3d062d6d
Adil-Bee200 <151982354+Adil-Bee200@users.noreply.github.com>
2025-08-14 20:20:51 +0500

    Updated main to add routers

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from core.config import settings
+from routers import job, story
 
 app = FastAPI(
     title= "Choose your own adventure API",
@@ -20,3 +21,6 @@ app.add_middleware(
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run("main:app", host= "0.0.0.0", port= 8000, reload= True)
+
+app.include_router(job.router, prefix=settings.API_PREFIX)
+app.include_router(story.router, prefix=settings.API_PREFIX)
